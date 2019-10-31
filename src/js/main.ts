1485e2f5594562309424b307279e114c16a45be7
MMBBAA <a-bedmarmto@outlook.com>
2019-10-31 21:57:23 +0100

    jeep project, running and displays an image

---
+++ src/js/main.ts
@@ -0,0 +1,31 @@
+import { Game, AUTO } from 'phaser';
+import { SceneOne } from './scenes/SceneOne';
+
+const mySettings = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    scene: [
+        SceneOne
+    ],
+    parent: 'my-container',
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    }
+}
+
+
+//only run when  the DOM Content is Loaded.
+
+document.addEventListener('DOMContentLoaded', () => {
+    let myContainerDiv = document.getElementById("my-container");//obtain a piece of html, now we can control it with js
+    myContainerDiv.innerHTML += '';
+
+    new Game(mySettings);
+});
---
+++ src/js/scenes/SceneOne.ts
@@ -0,0 +1,28 @@
+import { Scene, Physics } from 'phaser';
+//import { Jeep } from '../Jeep';
+
+export class SceneOne extends Scene {
+    iconobase: Phaser.GameObjects.Image | undefined;
+
+    constructor() {
+        super({
+            key: 'SceneOne'
+        })
+    }
+
+    preload() {
+        this.load.image('base', '../../../assets/images/iconobase.png');
+    }
+
+    create() {
+        this.iconobase = this.add.image(400, 300, 'base');
+    //    this.myLogo.setScale(0.3);
+
+   //this.jeep = new Jeep(this, 200, 400);
+    }
+
+    update() {
+   //     this.myLogo.angle += 12;
+        // this.myLogo.x += 0.5
+    }
+}
