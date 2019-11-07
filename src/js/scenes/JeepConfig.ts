16a1d2bd78f1ac2cdd0a8f3fd5a947f7fbe2b896
dericbytes <dericbytes@nowhere.com>
2019-11-07 10:31:23 +0000

    audio file paths now relative

--- src/js/scenes/JeepConfig.ts
+++ src/js/scenes/JeepConfig.ts
@@ -1,116 +1,117 @@
 export const JeepConfig = {
-    images_dir: '../../../assets/images/',
-    images: [
-      {
-        id: 'jeep_up',
-        direction: 'up',
-        obj: 'jeep',
-        src: 'jeepUp.png'
-      },
+  images_dir: '../../../assets/images/',
+  images: [
+    {
+      id: 'jeep_up',
+      direction: 'up',
+      obj: 'jeep',
+      src: 'jeepUp.png'
+    },
 
-      {
-        id: 'jeep_down',  
-        direction: 'down',
-        obj: 'jeep',
-        src: 'jeepDown.png'
-      },
+    {
+      id: 'jeep_down',
+      direction: 'down',
+      obj: 'jeep',
+      src: 'jeepDown.png'
+    },
 
-      {
-        id: 'jeep_left',  
-        direction: 'left',
-        obj: 'jeep',
-        src: 'jeepLeft.png'
-      },
+    {
+      id: 'jeep_left',
+      direction: 'left',
+      obj: 'jeep',
+      src: 'jeepLeft.png'
+    },
 
-      {
-        id: 'jeep_right',
-        direction: 'right',
-        obj: 'jeep',
-        src: 'jeepRight.png'
-      }
+    {
+      id: 'jeep_right',
+      direction: 'right',
+      obj: 'jeep',
+      src: 'jeepRight.png'
+    }
 
-    ],
-    audio: [
-      {
-        id: 'Jeep',
-        url: 'assets/music/Jeep_En_Marcha_3.mp3',
-        type: 'jeep'
-      },
-      {
-        id: 'Arranque',
-        url: 'assets/music/Jeep_Arrancando.mp3',
-        type: 'jeep'
-      },
-      {
-        id: 'Freno',
-        url: 'assets/music/Jeep_Frenando.mp3',
-        type: 'jeep'
-      },
-      {
-        id: 'BateriaBaja',
-        url: 'assets/music/Bateria_Baja.mp3',
-        type: 'jeep'
-      },
-      {
-        id: 'RecargaEnergia',
-        url: 'assets/music/Recarga_Energia.mp3',
-        type: 'jeep'
-      },
-      {
-        id: 'oxigenoBajo',
-        url: 'assets/music/Oxigeno_Bajo.mp3',
-        type: 'jeep',
-        loop: 'true'
-      },
-      {
-        id: 'Energia',
-        url: 'assets/music/Sonido_Energia_0.mp3',
-        type: 'bateria'
-      },
-      {
-        id: 'ImpactoHuracan2',
-        url: 'assets/music/Sonido_Colision_Huracan.mp3',
-        type: 'huracan'
-      },
-      {
-        id: 'ImpactoCrater',
-        url: 'assets/music/Sonido_Colision_Crater.mp3',
-        type: 'impact'
-      },
-      {
-        id: 'ImpactoHuracan',
-        url: 'assets/music/Impacto_Huracan.mp3',
-        type: 'impact'
-      },
+  ],
+  audio_dir: '../../../assets/music/',
+  audio: [
+    {
+      id: 'Jeep',
+      url: 'Jeep_En_Marcha_3.mp3',
+      type: 'jeep'
+    },
+    {
+      id: 'Arranque',
+      url: 'Jeep_Arrancando.mp3',
+      type: 'jeep'
+    },
+    {
+      id: 'Freno',
+      url: 'Jeep_Frenando.mp3',
+      type: 'jeep'
+    },
+    {
+      id: 'BateriaBaja',
+      url: 'Bateria_Baja.mp3',
+      type: 'jeep'
+    },
+    {
+      id: 'RecargaEnergia',
+      url: 'Recarga_Energia.mp3',
+      type: 'jeep'
+    },
+    {
+      id: 'oxigenoBajo',
+      url: 'Oxigeno_Bajo.mp3',
+      type: 'jeep',
+      loop: 'true'
+    },
+    {
+      id: 'Energia',
+      url: 'Sonido_Energia_0.mp3',
+      type: 'bateria'
+    },
+    {
+      id: 'ImpactoHuracan2',
+      url: 'Sonido_Colision_Huracan.mp3',
+      type: 'huracan'
+    },
+    {
+      id: 'ImpactoCrater',
+      url: 'Sonido_Colision_Crater.mp3',
+      type: 'impact'
+    },
+    {
+      id: 'ImpactoHuracan',
+      url: 'Impacto_Huracan.mp3',
+      type: 'impact'
+    },
 
-      {
-        id: 'VueltaBase',
-        url: 'assets/music/Traer_Jeep_Vuelta_A_Base.mp3',
-        type: 'base'
-      },
-      {
-        id: 'VueltaBase2',
-        url: 'assets/music/Traer_Jeep_Vuelta_A_Base.mp3',
-        type: 'base'
-      },
-      {
-        id: 'MuestraRecogida',
-        url: 'assets/music/Muestra_Recogida.mp3',
-        type: 'muestra'
-      },
-      {
-        id: 'EscudosBajos',
-        url: 'assets/music/Escudos_Bajos.mp3',
-        type: 'escudos'
-      },
-      {
-        id: 'EscudosBajos2',
-        url: 'assets/music/Sonido_Escudos_Bajos.mp3',
-        type: 'escudos'
-      },
-      {
-        id: 'ShieldReload',
-        url: 'assets/music/Sonido_Recarga_Escudos.mp3',
-        type: 'escudos'
-      }]
-  };
+    {
+      id: 'VueltaBase',
+      url: 'Traer_Jeep_Vuelta_A_Base.mp3',
+      type: 'base'
+    },
+    {
+      id: 'VueltaBase2',
+      url: 'Traer_Jeep_Vuelta_A_Base.mp3',
+      type: 'base'
+    },
+    {
+      id: 'MuestraRecogida',
+      url: 'Muestra_Recogida.mp3',
+      type: 'muestra'
+    },
+    {
+      id: 'EscudosBajos',
+      url: 'Escudos_Bajos.mp3',
+      type: 'escudos'
+    },
+    {
+      id: 'EscudosBajos2',
+      url: 'Sonido_Escudos_Bajos.mp3',
+      type: 'escudos'
+    },
+    {
+      id: 'ShieldReload',
+      url: 'Sonido_Recarga_Escudos.mp3',
+      type: 'escudos'
+    }]
+};
